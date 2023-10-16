67c644a9d788872da2bf5f196fd38e9db74306f5
Adi-UA <abanerjee.email@gmail.com>
2023-10-15 18:00:13 -0700

    Added results

--- scripts/get_im2latex100k_results.py
+++ scripts/get_im2latex100k_results.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -6,12 +7,28 @@ data_dir = os.path.join(project_dir, "dataset/data")
 with open(os.path.join(data_dir, "tgt-test.txt"), "r") as f:
     train_formulae = [formula.rstrip() for formula in f.readlines()]
 
-with open(os.path.join(data_dir, "src-train.txt"), "r") as f:
+with open(os.path.join(data_dir, "src-test.txt"), "r") as f:
     train_img_names = [img_name.rstrip().rstrip(".png") for img_name in f.readlines()]
 
 with open(os.path.join(project_dir, "pred.txt"), "r") as f:
     pred_formulae = [formula.rstrip() for formula in f.readlines()]
 
-print(len(train_formulae), len(train_img_names), len(pred_formulae))
 
 results = {}
+for img_name, formula, pred_formula in zip(
+    train_img_names, train_formulae, pred_formulae
+):
+    results[img_name] = {
+        "prediction": pred_formula,
+        "ground_truth": formula,
+    }
+
+# Create results directory if it doesn't exist
+if not os.path.exists(os.path.join(project_dir, "results")):
+    os.mkdir(os.path.join(project_dir, "results"))
+
+results_dir = os.path.join(project_dir, "results")
+
+# Save results
+with open(os.path.join(results_dir, "im2latex100k_test_results.json"), "w") as f:
+    json.dump(results, f, indent=4)
